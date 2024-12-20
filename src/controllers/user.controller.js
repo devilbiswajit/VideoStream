11abe322ffac9492ee97f8ba3d4add203ec0e6cb
devilbiswajit <biswajitranachagulu13524612@gmail.com>
2024-12-20 17:25:16 +0530

    Adding more models and updating user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -145,9 +145,14 @@ const loginUser = asyncHandler(async(req,res)=>{
 const logoutUser = asyncHandler(async(req,res)=>{
     await User.findByIdAndUpdate(
      req.user._id,
-     {
-       $set:{ refreshToken:undefined}
-     },
+   //   {
+   //     $set:{ refreshToken:NULL}
+   //   },
+   //  or
+   {
+       $unset:{refreshToken:1}
+      
+   },
       
      {
         new:true
